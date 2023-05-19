797434bc27b532df7e1dbbb0f82f7096500a4c15
Maxsos133 <max.pavlenko133@gmail.com>
2023-05-19 14:22:10 -0500

    html and css styling, create a side panel when clicking on a hero, and have a video of the hero inside on autoplay

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 const steamApiKey = `9720CE9B302ADC1F7B65B4E28D8D9196`
 const apiKey = `c120c72b-dcac-41c1-b92b-dbaa1ac4ffc5`
 let $heroesDiv = $(`.heroesDiv`)
+let $sidePanel = $(`.side-panel`)
 
 
 async function drawHeroes() {
     let response = await axios.get(`https://api.opendota.com/api/heroes?api_key=${apiKey}`)
     console.log(response.data)
     for (let i = 0; i < response.data.length; i++) {
-        let heroGrid = `<div id="${response.data[i].localized_name}">
-        <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${response.data[i].name.toLowerCase().replaceAll('npc_dota_hero_', '')}.png"/>
+        let actualName = response.data[i].name.toLowerCase().replaceAll('npc_dota_hero_', '')
+        let heroGrid = `<div class="eachHero "id="${actualName}">
+        <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${actualName}.png"/>
         ${response.data[i].localized_name}
         </div>`
-
         $heroesDiv.append(heroGrid)
-        
-        // } else if(response.data[i].localized_name === `Anti-Mage`){
-        //     let heroGrid = `<div id="${response.data[i].localized_name}">
-        // <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/antimage.png"/>
-        // ${response.data[i].localized_name}
-        // </div>`
+        let $picId = $(`#${actualName}`)
 
-        // $heroesDiv.append(heroGrid)
-        // } else if(response.data[i].localized_name === `Anti-Mage`){
-        //     let heroGrid = `<div id="${response.data[i].localized_name}">
-        // <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/antimage.png"/>
-        // ${response.data[i].localized_name}
-        // </div>`
+        $picId.on(`click`, function() {
+            console.log(actualName)
+            document.querySelector(".wrapper").classList.toggle("side-panel-open")
+            let heroSidePanel = `
+            <video poster="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${actualName}.png" 
+            autoplay="" preload="auto" loop="" playsinline="">
+            <source type="video/webm" src="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${actualName}.webm">
+            <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${actualName}.png"></video>
 
-        // $heroesDiv.append(heroGrid)
-        // }
-    
-}
+
+            <h3>${response.data[i].localized_name}</h3>
+            
+            
+            
+            `
+            $sidePanel.html(heroSidePanel)
+        })
+
+
+    }
 }
 drawHeroes()
+
