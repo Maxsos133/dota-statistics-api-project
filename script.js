1b6839858462bed9a13520119c776e4f736c8ffd
Maxsos133 <max.pavlenko133@gmail.com>
2023-05-19 11:45:21 -0500

    fix error antimage error

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ async function drawHeroes() {
     let response = await axios.get(`https://api.opendota.com/api/heroes?api_key=${apiKey}`)
     console.log(response.data)
     for (let i = 0; i < response.data.length; i++) {
-        if (response.data[i].localized_name != `Anti-Mage`) {
         let heroGrid = `<div id="${response.data[i].localized_name}">
         <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${response.data[i].name.toLowerCase().replaceAll('npc_dota_hero_', '')}.png"/>
         ${response.data[i].localized_name}
         </div>`
 
         $heroesDiv.append(heroGrid)
-        }
+        
         // } else if(response.data[i].localized_name === `Anti-Mage`){
         //     let heroGrid = `<div id="${response.data[i].localized_name}">
         // <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/antimage.png"/>
